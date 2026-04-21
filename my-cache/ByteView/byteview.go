9238da020076a662f2c76eb74e78b84b6d095d19
zanen <zanen@1229448879@qq.com>
2026-04-21 17:16:48 +0000

    byteView的抽象数据结构的封装

---
+++ my-cache/ByteView/byteview.go
@@ -0,0 +1,23 @@
+package byteview
+
+//对真正存储底层数据的b []byte进行封装，提供一个只读的拷贝切片给外部
+//实现真正的只读保护
+
+type ByteView struct {
+	b []byte
+}
+
+func (v ByteView) Len() int {
+	return len(v.b)
+}
+
+func (v ByteView) ByteSlices() []byte {
+	res := make([]byte, len(v.b))
+	copy(res, v.b)
+	return res
+}
+
+// 输出时输出v.String()确保输出字符串而非底层的字节
+func (v ByteView) String() string {
+	return string(v.b)
+}
