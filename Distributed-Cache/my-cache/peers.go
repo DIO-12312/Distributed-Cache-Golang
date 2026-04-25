7b45a8762b1cb66362256ef052d91d2e886f5878
zanen <zanen@1229448879@qq.com>
2026-04-25 04:40:42 +0000

    Peer接口

--- Distributed-Cache/my-cache/peers.go
+++ Distributed-Cache/my-cache/peers.go
@@ -1,5 +1,13 @@
 package mycache
 
+// 1. 选择节点的策略
 type PeerPicker interface {
-	PeerPicker
+	PickPeer(key string) (peer PeerGetter, ok bool)
 }
+
+// 2. 获取节点数据的方法
+type PeerGetter interface {
+	Get(group string, key string) ([]byte, error)
+}
+
+//可以分别改进选择策略和通信方式 ，降低耦合性
